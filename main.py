345fb1553dc4f2cc5a87b4fb8fc4b65076162873
Shourya306 <shouryareddy306@gmail.com>
2022-10-04 09:17:07 +0530

    Commiting the first version of app and final version of requirements.txt

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
 from fastapi import FastAPI
-from typing import Optional
 from pydantic import BaseModel
+import pickle
 
 app = FastAPI()
+model = pickle.load(open('/linear_model_fastapi.pkl','rb'))
+
 
 @app.get('/')
-def index(published: bool,limit: int = 10, sort: Optional[str] = None) :
-    if published:
-        return {'data':f'{limit} published blogs'}
-    else:
-        return {'data':f'{limit} blogs'}
+def home():
+    return 'Home page'
 
-@app.get('/blog/unpublished')
-def unpublished():
-    return 'These are the list of unpublished blogs'
-
-@app.get('/blog/{id}')
-def about(id: int):
-    return {'data':id}
-
-@app.get('/blog/{id}/comments')
-def comments(id: int):
-    return {id:'This is the comment for this blog'}
-
-
-class blog(BaseModel):
-    title : str
-    body : str
-    published_date : Optional[bool]
-
-@app.post('/predict')
-def predict(request: blog):
-    return request
